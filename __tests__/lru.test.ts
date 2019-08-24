0a8413e600f9648a5e0d3df69906b79830ac9d04
Tino Vyatkin <tino@vtkn.io>
2019-08-24 01:02:20 -0400

    LRU test complete

--- __tests__/lru.test.ts
+++ __tests__/lru.test.ts
@@ -73,33 +73,22 @@ describe('Use TLRU as LRU', () => {
     expect(lru.size).toBe(2);
     expect([...lru.keys()]).toEqual(['foo1', 'foo3']);
   });
-});
 
-/*
-
-
-
-
-
-
-
-suite.addBatch({
-
-  'lru invariant is maintained after set(), get() and remove()': function() {
-    var lru = new LRU(2);
+  it('lru invariant is maintained after set(), get() and remove()', async () => {
+    const lru = new TLRU({ defaultLRU: true, maxStoreSize: 2 });
     lru.set('a', 1);
     lru.set('b', 2);
-    assert.deepEqual(lru.get('a'), 1);
-    lru.remove('a');
+    expect(lru.get('a')).toBe(1);
+    lru.delete('a');
     lru.set('c', 1);
     lru.set('d', 1);
-    assert.deepEqual(['c', 'd'], lru.keys);
-  }
-});
 
-suite.addBatch({
-  'lru invariant is maintained in the corner case size == 1': function() {
-    var lru = new LRU(1);
+    await new Promise(resolve => setImmediate(resolve));
+    expect([...lru.keys()]).toEqual(['c', 'd']);
+  });
+
+  it('lru invariant is maintained in the corner case size == 1', async () => {
+    const lru = new TLRU({ defaultLRU: true, maxStoreSize: 1 });
 
     lru.set('foo1', 'bar1');
     lru.set('foo2', 'bar2');
@@ -108,163 +97,115 @@ suite.addBatch({
 
     lru.set('foo3', 'bar3');
 
-    assert.deepEqual(['foo3'], lru.keys);
-  }
-});
+    await new Promise(resolve => setImmediate(resolve));
+    expect([...lru.keys()]).toEqual(['foo3']);
+  });
 
-suite.addBatch({
-  'get() returns item value': function() {
-    var lru = new LRU(2);
+  it('get() returns item value', () => {
+    const lru = new TLRU({ defaultLRU: true, maxStoreSize: 2 });
+    lru.set('foo', 'bar');
+    expect(lru.get('foo')).toBe('bar');
+  });
 
-    assert.equal(lru.set('foo', 'bar'), 'bar');
-  }
-});
-
-suite.addBatch({
-  'peek() returns item value without changing the order': function() {
-    var lru = new LRU(2);
+  it('get() with revieve=false returns item value without changing the order', async () => {
+    const lru = new TLRU({ defaultLRU: true, maxStoreSize: 2 });
     lru.set('foo', 'bar');
     lru.set('bar', 'baz');
-    assert.equal(lru.peek('foo'), 'bar');
+    expect(lru.get('foo', false)).toEqual('bar');
     lru.set('baz', 'foo');
-    assert.equal(lru.get('foo'), null);
-  }
+
+    await new Promise(resolve => setImmediate(resolve));
+    expect(lru.get('foo')).toBeUndefined();
+  });
+
+  it('get respects max age', async () => {
+    const lru = new TLRU({ defaultLRU: true, maxAgeMs: 5 });
+    lru.set('foo', 'bar');
+    expect(lru.get('foo')).toBe('bar');
+    await new Promise(resolve => setTimeout(resolve, 100));
+    //   'the entry is removed if age > max_age'
+    expect(lru.has('foo')).toBeFalsy();
+  }, 300);
+
+  it('evicting items by age', async () => {
+    const lru = new TLRU({ defaultLRU: true, maxAgeMs: 5 });
+    lru.set('foo', 'bar');
+    expect(lru.get('foo')).toBe('bar');
+    await new Promise(resolve => setTimeout(resolve, 100));
+    // 'the entry is removed if age > max_age': function(lru) {
+    expect(lru.get('foo')).toBeUndefined();
+  });
+
+  it('evicting items by age (2)', async () => {
+    const lru = new TLRU({ defaultLRU: true, maxAgeMs: 100000 });
+    lru.set('foo', 'bar');
+    expect(lru.get('foo')).toBe('bar');
+    await new Promise(resolve => setTimeout(resolve, 100));
+    // the entry is not removed if age < max_age': function(lru) {
+    expect(lru.get('foo')).toBe('bar');
+  });
 });
 
-suite.addBatch({
-  'peek respects max age': {
-    topic: function() {
-      var lru = new LRU({ maxAge: 5 });
-      lru.set('foo', 'bar');
-      assert.equal(lru.get('foo'), 'bar');
-      var callback = this.callback;
-      setTimeout(function() {
-        callback(null, lru);
-      }, 100);
-    },
-    'the entry is removed if age > max_age': function(lru) {
-      assert.equal(lru.peek('foo'), null);
-    }
-  }
+describe('idempotent changes', () => {
+  afterEach(setImmediate);
+
+  it('set() and remove() on empty LRU is idempotent', () => {
+    const lru = new TLRU({ defaultLRU: true });
+    const json1 = JSON.stringify(lru);
+
+    lru.set('foo1', 'bar1');
+    lru.delete('foo1');
+    const json2 = JSON.stringify(lru);
+
+    expect(json2).toBe(json1);
+  });
+
+  it('2 set()s and 2 remove()s on empty LRU is idempotent', () => {
+    const lru = new TLRU({ defaultLRU: true });
+    const json1 = JSON.stringify(lru);
+
+    lru.set('foo1', 'bar1');
+    lru.set('foo2', 'bar2');
+    lru.delete('foo1');
+    lru.delete('foo2');
+    const json2 = JSON.stringify(lru);
+
+    expect(json2).toBe(json1);
+  });
+
+  it('2 set()s and 2 remove()s (in opposite order) on empty LRU is idempotent', () => {
+    const lru = new TLRU({ defaultLRU: true });
+    const json1 = JSON.stringify(lru);
+
+    lru.set('foo1', 'bar1');
+    lru.set('foo2', 'bar2');
+    lru.delete('foo2');
+    lru.delete('foo1');
+    const json2 = JSON.stringify(lru);
+
+    expect(json2).toBe(json1);
+  });
+
+  it('after setting one key, get() is idempotent', () => {
+    const lru = new TLRU({ defaultLRU: true, maxStoreSize: 2 });
+    lru.set('a', 'a');
+    const json1 = JSON.stringify(lru);
+
+    lru.get('a');
+    const json2 = JSON.stringify(lru);
+
+    expect(json2).toBe(json1);
+  });
+
+  it('after setting two keys, get() on last-set key is idempotent', () => {
+    const lru = new TLRU({ defaultLRU: true, maxStoreSize: 2 });
+    lru.set('a', 'a');
+    lru.set('b', 'b');
+    const json1 = JSON.stringify(lru);
+
+    lru.get('b');
+    const json2 = JSON.stringify(lru);
+
+    expect(json2).toBe(json1);
+  });
 });
-
-suite.addBatch({
-  'evicting items by age': {
-    topic: function() {
-      var lru = new LRU({ maxAge: 5 });
-      lru.set('foo', 'bar');
-      assert.equal(lru.get('foo'), 'bar');
-      var callback = this.callback;
-      setTimeout(function() {
-        callback(null, lru);
-      }, 100);
-    },
-    'the entry is removed if age > max_age': function(lru) {
-      assert.equal(lru.get('foo'), null);
-    }
-  },
-  'evicting items by age (2)': {
-    topic: function() {
-      var lru = new LRU({ maxAge: 100000 });
-      lru.set('foo', 'bar');
-      assert.equal(lru.get('foo'), 'bar');
-      var callback = this.callback;
-      setTimeout(function() {
-        callback(null, lru);
-      }, 100);
-    },
-    'the entry is not removed if age < max_age': function(lru) {
-      assert.equal(lru.get('foo'), 'bar');
-    }
-  }
-});
-
-suite.addBatch({
-  'idempotent changes': {
-    'set() and remove() on empty LRU is idempotent': function() {
-      var lru = new LRU();
-      var json1 = JSON.stringify(lru);
-
-      lru.set('foo1', 'bar1');
-      lru.remove('foo1');
-      var json2 = JSON.stringify(lru);
-
-      assert.deepEqual(json2, json1);
-    },
-
-    '2 set()s and 2 remove()s on empty LRU is idempotent': function() {
-      var lru = new LRU();
-      var json1 = JSON.stringify(lru);
-
-      lru.set('foo1', 'bar1');
-      lru.set('foo2', 'bar2');
-      lru.remove('foo1');
-      lru.remove('foo2');
-      var json2 = JSON.stringify(lru);
-
-      assert.deepEqual(json2, json1);
-    },
-
-    '2 set()s and 2 remove()s (in opposite order) on empty LRU is idempotent': function() {
-      var lru = new LRU();
-      var json1 = JSON.stringify(lru);
-
-      lru.set('foo1', 'bar1');
-      lru.set('foo2', 'bar2');
-      lru.remove('foo2');
-      lru.remove('foo1');
-      var json2 = JSON.stringify(lru);
-
-      assert.deepEqual(json2, json1);
-    },
-
-    'after setting one key, get() is idempotent': function() {
-      var lru = new LRU(2);
-      lru.set('a', 'a');
-      var json1 = JSON.stringify(lru);
-
-      lru.get('a');
-      var json2 = JSON.stringify(lru);
-
-      assert.equal(json2, json1);
-    },
-
-    'after setting two keys, get() on last-set key is idempotent': function() {
-      var lru = new LRU(2);
-      lru.set('a', 'a');
-      lru.set('b', 'b');
-      var json1 = JSON.stringify(lru);
-
-      lru.get('b');
-      var json2 = JSON.stringify(lru);
-
-      assert.equal(json2, json1);
-    }
-  }
-});
-
-suite.addBatch({
-  'evict event': {
-    "'evict' event is fired when evicting old keys": function() {
-      var lru = new LRU(2);
-      var events = [];
-      lru.on('evict', function(element) {
-        events.push(element);
-      });
-
-      lru.set('foo1', 'bar1');
-      lru.set('foo2', 'bar2');
-      lru.set('foo3', 'bar3');
-      lru.set('foo4', 'bar4');
-
-      var expect = [
-        { key: 'foo1', value: 'bar1' },
-        { key: 'foo2', value: 'bar2' }
-      ];
-      assert.deepEqual(events, expect);
-    }
-  }
-});
-
-suite.export(module);
-*/
